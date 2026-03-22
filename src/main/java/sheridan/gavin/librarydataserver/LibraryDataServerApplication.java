671b88b853c60e308d823ef9bd6ce20ece788c22
Gavin Blackie <blackieg@sheridancollege.ca>
2026-03-22 16:20:29 -0400

    Created/Initialized Part 1 - LibraryDataServer using Intellij settings

---
+++ src/main/java/sheridan/gavin/librarydataserver/LibraryDataServerApplication.java
@@ -0,0 +1,13 @@
+package sheridan.gavin.librarydataserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryDataServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryDataServerApplication.class, args);
+	}
+
+}
