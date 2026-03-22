74c4e39b314b3b3460ea2cfa952a8b1ef791c318
Gavin Blackie <blackieg@sheridancollege.ca>
2026-03-22 16:35:54 -0400

    Added the OpenAPI config for SwaggerUI (just displays the name, version, and description of this API)

---
+++ src/main/java/sheridan/gavin/librarydataserver/config/OpenAPIConfig.java
@@ -0,0 +1,22 @@
+package sheridan.gavin.librarydataserver.config;
+
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+// This config is primarily for showing what displays on SwaggerUI!
+@Configuration
+public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Library Data API")
+                        .version("1.0")
+                        .description("API for accessing book data!")
+                );
+    }
+}
