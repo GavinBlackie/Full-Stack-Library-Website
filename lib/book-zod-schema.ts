81370c7cdfec70c5adf2559dcbce9475d2d875d5
Gavin Blackie <blackieg@sheridancollege.ca>
2026-04-11 15:18:43 -0400

    Edited zod error messages, also added some more constraints including a regex for ISBNs!

--- lib/book-zod-schema.ts
+++ lib/book-zod-schema.ts
@@ -3,11 +3,17 @@ import * as z from "zod";
 // A book schema made of Zod objects (for the forms),
 export const bookSchema = z.object({
     itemId: z
-        .string().min(4, {message: "Book Id must be at least 4 characters"}),
+        .string()
+        .min(4, {message: "Book Id must be at least 4 characters. "})
+        .max(30, {message: "Book Id cannot be more than 30 characters. "}),
     isbn: z
-        .string().min(4, {message: "ISBN must have at least 4 characters"}),
+        .string()
+        .min(4, {message: "ISBN must have at least 4 characters. "})
+        .max(30, {message: "ISBN cannot have more than 30 characters. "})
+        .regex(/\d+-\d+/, {message: "ISBN must be some #s, then a dash (-), then some #s. "}),
     bookTitle: z
-        .string().min(2, {message: "Must have at least 2 characters for title"} ),
+        .string().
+        min(2, {message: "Must have at least 2 characters for title. "} ),
     pageCount: z.coerce
         .number<number>("Page Count must be a number. ")
         .int("Page Count must be a whole number. ")
@@ -16,5 +22,5 @@ export const bookSchema = z.object({
         .boolean(),
     lateFeeUsd: z.coerce
         .number<number>("Late fee must numeric. ")
-        .min(0, "Area cannot be negative. "),
+        .min(0, "Late fee cannot be negative. "),
 });
